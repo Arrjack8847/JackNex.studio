5dfefb75e0c661fdec6e661b1caefbb6e04099a9
Arrjack8847 <smks8847@gmail.com>
2026-04-11 19:46:38 +0800

    Update image source in About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -45,7 +45,7 @@ const About = () => {
             >
               {/* image */}
               <motion.img
-  src="public/hero-man.png"
+  src="public/hero-man1.png"
   alt="Portrait"
   className="h-full w-full object-cover grayscale transition-all duration-700 group-hover:grayscale-0"
   animate={{ scale: [1, 1.03, 1] }}
